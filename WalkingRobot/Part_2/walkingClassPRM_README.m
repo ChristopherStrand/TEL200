197ce6ef1eb31bdbd43959929949b6a77b701fd7
Ludvik Høibjerg-Aslaksen <Ludvik253@gmail.com>
2025-04-16 11:24:51 +0200

    Feat: Added documentation to README
    - Added doucmentation in walkingClassPRM_README
    - Removed old test file

--- WalkingRobot/Part_2/walkingClassPRM_README.m
+++ WalkingRobot/Part_2/walkingClassPRM_README.m
@@ -2,11 +2,41 @@ import walkingClassPRM.*;
 import WalkingPRM.*;
 
 hold on
-my_paths = WalkingPRM(200, 1);
-% Reverse z-axis on prm plot
-% Needs to be placed after PRM has been generated
+%{
+Warning! Before running this code make sure to
+add Part_1 and Part_2 to your path.
+
+WalkingPRM handles all code related to making and plotting paths. Currently
+supports only plotting the latest generated path. Inside the WalkingPRM
+file you can set a custom seed and load a different map. 
+WalkingPRM has two parameters:
+    - Amount of dots (Large numbers take a lot of time)
+    - Amount of paths to generate
+WalkingPRM outputs all paths generated
+%}
+my_paths = WalkingPRM(200, 1); % Handles PRM plotting
+
+% The robot expects a reverse z-axis se we reverse the z-axis of the PRM
+% plot
 set(gca, 'Zdir', 'reverse'); 
 
+%{
+WalkingClassPRM is an version of walkingClassPrivateCoords adjusted for use
+in plotting on top of a different plot. By calling robot_animate you
+can manually move the robot forward or turn it.
+Example:
+W1 = walkingClassPrivateCoords(500, "nah", false);
+W1.robot_animate(@W1.turn, 90); 
+W1.robot_animate(@W1.move, 100);
+
+The robot can also follow a generated path automatically by calling follow.
+Follow takes a single path or multiple paths of any size then moves the
+robot to each point in the path. Warning! Untested with multiple paths
+Example:
+W1 = walkingClassPRM(250, "movie_test.mp4", false); % Handles robot movement
+W1.follow(my_paths);
+%}
+
 % Example for how to make a robot object and then follow a generated path
-W1 = walkingClassPRM(250, "movie_test.mp4", false);
+W1 = walkingClassPRM(250, "movie_test.mp4", false); % Handles robot movement
 W1.follow(my_paths);
--- WalkingRobot/test_delete.m
+++
@@ -1,4 +0,0 @@
-my_array = {'a', 'b', 'c'};
-for i = 1:length(my_array)
-    disp([num2str(i) ' ' my_array{i}]);
-end
